e491fd07572ea23b1746936a8867a1686e3194f3
BurdetteLamar <burdettelamar@yahoo.com>
2021-03-30 16:34:10 -0500

    More on tutorial

---
+++ doc/ruby/optional_argument.rb
@@ -0,0 +1,9 @@
+require 'optparse'
+parser = OptionParser.new
+parser.on('-x [XXX]', '--xxx') do |option|
+  p "--xxx #{option}"
+end
+parser.on('-y', '--y [YYY]') do |option|
+  p "--yyy #{option}"
+end
+parser.parse!
